b82c04d78f3f4e7d8159fb15d10c2e07d7d4186a
Joris <jorisjosephl@gmail.com>
2023-12-04 09:55:09 +0100

    ajout liste de tirettes

--- projet1.py
+++ projet1.py
@@ -16,15 +16,12 @@ def tirette_aleatoire():
         tirettes.append(ligne)
         i+=1
     return tirettes
-        
-def tirettes():
-    tirette_droite = tirette_aleatoire()
-    tirette_gauche = tirette_aleatoire()
-    tirette_haut = tirette_aleatoire()
-    tirette_bas = tirette_aleatoire()
-    lst_tirettes = [tirette_gauche,tirette_droite,tirette_haut,tirette_bas]
-    return lst_tirettes
-    
-tirettes = tirettes() #liste de liste: tirette gauche, droite, haut, bas
+     
+tirette_droite = tirette_aleatoire()
+tirette_gauche = tirette_aleatoire()
+tirette_haut = tirette_aleatoire()
+tirette_bas = tirette_aleatoire()
+
+
 
 
