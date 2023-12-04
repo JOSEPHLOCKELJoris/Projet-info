e1cd19420718e0bc6d78cb8b115733105a40e649
Joris <jorisjosephl@gmail.com>
2023-12-04 09:53:47 +0100

    ajout liste de tirettes

--- projet1.py
+++ projet1.py
@@ -1,12 +1,30 @@
+from random import randint
+from fltk import *
 
-
+def tirette_aleatoire():
+    tirettes = []
+    i = 0
+    while i < 7:
+        ligne = []
+        for tirette in range(7):
+            alea = randint(0,1)
+            if alea == 0:
+                alea = False
+            else:
+                alea = True
+            ligne.append(alea)
+        tirettes.append(ligne)
+        i+=1
+    return tirettes
+        
 def tirettes():
-    tirette_droite = [[True,True,True,True,False,False,True],[True,False,True,False,True,False],[False,False,True,False,False,True,True],[True,False,False,True,True,True,False],[False,True,False,False,False,False,True],[False,False,True,True,False,False,True],[True,False,True,True,True,True,False]]
-    tirette_gauche = [[True,False,True,False,True,False],[False,True,False,False,False,False,True],[False,False,True,False,False,True,True],[True,False,False,True,True,True,False],[True,True,True,True,False,False,True],[False,False,True,True,False,False,True],[True,False,True,True,True,True,False]]
-    tirette_haut = [[True,False,True,False,True,False],[False,False,True,False,False,True,True],[True,True,True,True,False,False,True],[True,False,False,True,True,True,False],[False,True,False,False,False,False,True],[True,False,True,True,True,True,False],[False,False,True,True,False,False,True]]
-    tirette_bas = [[True,True,True,True,False,False,True],[False,False,True,False,False,True,True],[True,False,True,False,True,False],[True,False,False,True,True,True,False],[False,True,False,False,False,False,True],[True,False,True,True,True,True,False],[False,False,True,True,False,False,True]]
+    tirette_droite = tirette_aleatoire()
+    tirette_gauche = tirette_aleatoire()
+    tirette_haut = tirette_aleatoire()
+    tirette_bas = tirette_aleatoire()
     lst_tirettes = [tirette_gauche,tirette_droite,tirette_haut,tirette_bas]
     return lst_tirettes
     
-tirettes()
+tirettes = tirettes() #liste de liste: tirette gauche, droite, haut, bas
+
 
