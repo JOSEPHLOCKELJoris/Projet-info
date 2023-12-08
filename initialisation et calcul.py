37d5b604184b97cd4948a75e21042c69d94ba6a6
Joris <jorisjosephl@gmail.com>
2023-12-08 12:03:22 +0100

    fonctions mouvement des tirettes fait

--- initialisation et calcul.py
+++ initialisation et calcul.py
@@ -1,3 +1,6 @@
+from random import randint
+from fltk import *
+
 def tirette_aleatoire():
     tirettes = []
     i = 0
@@ -19,40 +22,62 @@ True = Trou; False = Plat(la boule ne peut pas tomber!)
 ex: True + True = La boule tombe
 """
 
+def placement_aleatoire(tout):
+    liste = []
+    for tout in tirettes:
+        position = []
+        for hori_verti in tout:
+            alea = randint(0,2)
+            position.append(alea)
+        liste.append(position)
+    return liste
+
 def all_tirettes():
     tirettes_horizontal = tirette_aleatoire()
     tirettes_verticale = tirette_aleatoire()
     tirettes = [tirettes_horizontal,tirettes_verticale]
     return tirettes
 
-def choix_tirettes(tirettes):
-    print("Il existe 2 types de tirettes: les verticales et les horizontales.")
+def possibilites(position_ex):
+    if position_ex == 0:
+        print("Vous pouvez uniquement tirez à gauche. Vous tirez donc à gauche. Action effectuée. ")
+        return 1
+    elif position_ex == 1:
+        print("Vous pouvez tirez à droite ou à gauche. ")
+        choix = input("Tapez d pout tirez à droite, g à gauche")
+        if choix == "d":
+            print("Vous tirez à droite. Action effectuée. ")
+            return 0
+        elif choix == "g":
+            print("Vous tirez à gauche. Action effectuée. ")
+            return 2
+    elif position_ex == 2:
+        print("Vous pouvez uniquemennt tirez à droite. Vous tirez donc à droite. Action effectuée. ")
+        return 1
+    
+def choix_tirettes(positions):
+    print("Il existe 2 types de tirettes: les verticales et les horizontales. ")
     sens = input("Quelles type de tirettes voulez-vous bouger? Tapez 'v' ou 'h'. ")
     print("Le rang se fait de haut en bas et de droite à gauche. ")
     rang = int(input("Quelles tirettes choississez vous? Tapez un chiffre de 0 à 6 puis sur la touche entrez. "))
     if sens == 'h':
         sens = 0
-        tirettes[sens][rang] = bouge_tirettes(tirettes[sens][rang]) #reconfig tirette select
+        positions[sens][rang] = possibilites(positions[sens][rang])
     elif sens == 'v':
         sens = 1
-        tirettes[sens][rang] = bouge_tirettes(tirettes[sens][rang]) #reconfig tirette select
-    return tirettes
+        print(positions)
+        positions[sens][rang] = possibilites(positions[sens][rang])
+    return positions
 
-def bouge_tirettes(tirette):
-    
-    """Fonction donnant les possibilités de "tire" à droite et/ou à gauche""" #en cours
-    
-    print("Vous pouvez tirez d'un cran vers la gauche ou la droite.")
-    direct = input("Dans quel sens voulez-vous tirez? Tapez 'd' ou 'g'. ")
-    
-    """tirette bouge, reinitialisation"""#en cours
-    if direct == 'd':
-        pass
-    elif direct == 'g':
-        pass
-    
-    return tirette
+def reinitialisation(tirettes, position):
+    pass
 
+"""initialisation"""
+tirettes = all_tirettes() #[[tirettes_horizontales],[tirettes_verticales]]
+position = placement_aleatoire(tirettes) #position initiale de chaque tirettes
 
-tirettes = all_tirettes()
-choix_tirettes(tirettes)
+"""fonction à répéter"""
+position = choix_tirettes(position) #choix et direction de la tirette
+print(position)
+reinitialisation(tirettes, position)# réinitialisation du plateau
+
