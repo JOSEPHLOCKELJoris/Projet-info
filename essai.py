62f26646b3cd9770bb4246e39941c8f5cc75bb4d
ELMERA TALENS <talens9460@gmail.com>
2023-12-08 12:33:59 +0100

    modification de quelque truc avec l'ajout de l'image des regles du jeu et un essai qui ne sert pas vraiment a grand chose

---
+++ essai.py
@@ -0,0 +1,14 @@
+from fltk import*
+
+largeur = 800
+hauteur = 800
+
+cree_fenetre(largeur, hauteur)
+attend_ev()
+ferme_fenetre()
+
+"""""
+def nouvelle_grille(n):
+    lst=[]
+    for _ in range(n)
+"""
